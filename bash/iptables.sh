973750a7d06ee449d923fb07ce60937bfc9b313e
Ah-Lun Tang <tang@ahlun.be>
2012-12-19 18:02:56 +0100

    external router with firewall, internal router with firewall and masquerading, bridging on bastion and bastion between firewalls in dmz
    Signed-off-by: Ah-Lun Tang <tang@ahlun.be>

--- bash/iptables.sh
+++ bash/iptables.sh
@@ -3,254 +3,239 @@
 ### BEGIN INFO
 # Name: Iptables Configuration Script
 # Author: Ah-Lun Tang <tang@ahlun.be>
-# Provides: iptables configuration for gateway
-# Short-Description: iptables script for gateway
+# Provides: iptables configuration
+# Short-Description: iptables script
 ### END INFO
 
-#IPTables Location
+#!/bin/bash
+
+# External Router
+EXT_INET_IFACE="eth0"
+EXT_INET_IP=`ifconfig $EXT_INET_IFACE | grep 'inet addr' | awk '{print $2}' | sed -e 's/.*://'`
+EXT_INET_NET="192.168.16.0/24"
+EXT_INET_BCAST=`ifconfig $EXT_INET_IFACE | grep 'inet addr' | awk '{print $3}' | sed -e 's/.*://'`
+
+EXT_DMZ_IFACE="eth1"
+EXT_DMZ_ADDRESS="192.168.70.254"
+
+# Internal Router
+INT_DMZ_IFACE="eth0"
+INT_DMZ_IP="192.168.70.250"
+
+INT_LAN_IFACE="eth1"
+INT_LAN_IP="10.0.0.254"
+INT_LAN_NET="10.0.0.0/8"
+INT_LAN_BCAST="10.0.0.255"
 
 
-IPT="/sbin/iptables"
-IPTS="/sbin/iptables-save"
-IPTR="/sbin/iptables-restore"
+# DMZ AND BASTION_HOST
+DMZ_NET="192.168.70.0/24"
+DMZ_BCAST="192.168.70.255"
+DMZ_BASTIONHOST="192.168.70.1"
+DMZ_IFACE_EXT="eth1"
+DMZ_IFACE_INT="eth0"
 
-# Internet Interface
-INET_IFACE="eth0"
-INET_ADDRESS="172.23.100.109"
 
-# Local Interface Information
-LOCAL_IFACE="eth2"
-LOCAL_IP="192.168.1.1"
-LOCAL_NET="192.168.1.0/24"
-LOCAL_BCAST="192.168.1.255"
 
-# DMZ Interface Information
-DMZ_IFACE="eth1"
-DMZ_IP="192.168.2.1"
-DMZ_NET="192.168.2.0/24"
-DMZ_BCAST="192.168.2.255"
-
-# Virtual Private Network interface
-VPN_IFACE="tun0"
 
 # Localhost Interface
 LO_IFACE="lo"
 LO_IP="127.0.0.1"
 
-
+#
+#
+#
+#  INTERNET            External Router             Bastion Host             Internal Router             Client
+#  ------              -------------              ------------              -------------              -------
+# |      |------------| eth0 | eth1 |------------|   bridge   |------------| eth0 | eth1 |------------|       |
+#  ------              -------------              ------------              -------------              -------
+#
+#
 dropcurrentconfig() {
     # Flush all rules
-    $IPT -F
-    $IPT -t nat -F
-    $IPT -t mangle -F
+    iptables -F
+    iptables -t nat -F
+    iptables -t mangle -F
 
     # Erase all non-default chains
-    $IPT -X
-    $IPT -t nat -X
-    $IPT -t mangle -X
+    iptables -X
+    iptables -t nat -X
+    iptables -t mangle -X
 
-    # Reset Default Policies
-    droppolicy
-}
-
-droppolicy(){
-    # Reset Default  Drop Policies
-    $IPT -P INPUT DROP
-    $IPT -P FORWARD DROP
-    $IPT -P OUTPUT DROP
-    $IPT -t nat -P PREROUTING DROP
-    $IPT -t nat -P POSTROUTING DROP
-    $IPT -t nat -P OUTPUT DROP
-    $IPT -t mangle -P OUTPUT DROP
 }
 
 acceptpolicy(){
     # Reset Default Accept Policies
-    $IPT -P INPUT ACCEPT
-    $IPT -P FORWARD ACCEPT
-    $IPT -P OUTPUT ACCEPT
-    $IPT -t nat -P PREROUTING ACCEPT
-    $IPT -t nat -P POSTROUTING ACCEPT
-    $IPT -t nat -P OUTPUT ACCEPT
-    $IPT -t mangle -P OUTPUT ACCEPT
+    iptables -P INPUT ACCEPT
+    iptables -P FORWARD ACCEPT
+    iptables -P OUTPUT ACCEPT
 }
 
-initfirewall(){
+droppolicy(){
+    # Reset Default Drop Policies
+    iptables -P INPUT DROP
+    iptables -P FORWARD DROP
+    iptables -P OUTPUT DROP
+}
+
+logging(){
     # drop all traffic from/to unknown/invalid connections
-    $IPT -A INPUT -m state --state INVALID -j LOG --log-prefix "Invalid input packet: "
-    $IPT -A INPUT -m state --state INVALID -j DROP
+    iptables -A INPUT -m state --state INVALID -j LOG --log-prefix "Invalid input packet: "
+    iptables -A INPUT -m state --state INVALID -j DROP
 
-    $IPT -A FORWARD -m state --state INVALID -j LOG --log-prefix "Invalid forward packet: "
-    $IPT -A FORWARD -m state --state INVALID -j DROP
+    iptables -A FORWARD -m state --state INVALID -j LOG --log-prefix "Invalid forward packet: "
+    iptables -A FORWARD -m state --state INVALID -j DROP
 
-    $IPT -A OUTPUT -m state --state INVALID -j LOG --log-prefix "Invalid output packet: "
-    $IPT -A OUTPUT -m state --state INVALID -j DROP
-}
-
-initnat(){
-    echo 1 > /proc/sys/net/ipv4/ip_forward # Enable routing
-    #$IPT -A INPUT -i $INET_IFACE -m state --state ESTABLISHED,RELATED -j ACCEPT # accept traffic from the Internet
-    $IPT -t nat -A POSTROUTING -o $INET_IFACE -j MASQUERADE # use masquerading
-}
-
-initlan(){
-    $IPT -A FORWARD -i $INET_IFACE -o $LOCAL_IFACE -j ACCEPT # forward from Internet to LAN
-    $IPT -A FORWARD -i $LOCAL_IFACE -o $INET_IFACE -j ACCEPT # forward from LAN to Internet
-
-    $IPT -A INPUT -i $LOCAL_IFACE -j ACCEPT # accept input traffic from LAN
-}
-
-initDMZ() {
-    $IPT -A FORWARD -i $LOCAL_IFACE -o $DMZ_IFACE -j ACCEPT # forward from LAN to DMZ
-    $IPT -A FORWARD -i $DMZ_IFACE -o $LOCAL_IFACE -j ACCEPT # forward from DMZ to LAN
-    $IPT -A FORWARD -i $INET_IFACE -o $DMZ_IFACE -j ACCEPT # forward from Internet to DMZ
-    $IPT -A FORWARD -i $DMZ_IFACE -o $INET_IFACE -j ACCEPT # forward from DMZ to Internet
-
-    $IPT -A INPUT -i $DMZ_IFACE -j ACCEPT # accept input traffic from DMZ
+    iptables -A OUTPUT -m state --state INVALID -j LOG --log-prefix "Invalid output packet: "
+    iptables -A OUTPUT -m state --state INVALID -j DROP
 }
 
 
-initvpn(){
+external_router() {
+    #echo 1 > /proc/sys/net/ipv4/ip_forward # Enable routing
 
-    # forward between vpn and other interfaces
-    $IPT -A FORWARD -i $VPN_IFACE -o $INET_IFACE -j ACCEPT
-    $IPT -A FORWARD -i $VPN_IFACE -o $LOCAL_IFACE -j ACCEPT
-    $IPT -A FORWARD -i $INET_IFACE -o $VPN_IFACE -j ACCEPT
-    $IPT -A FORWARD -i $LOCAL_IFACE -o $VPN_IFACE -j ACCEPT
-    $IPT -A FORWARD -i $DMZ_IFACE -o $VPN_IFACE -j ACCEPT
-    $IPT -A FORWARD -i $VPN_IFACE -o $DMZ_IFACE -j ACCEPT 
+    # prevent freezes
+    iptables -I INPUT -d $LO_IP -j ACCEPT
+    iptables -I OUTPUT -s $LO_IP -j ACCEPT
 
-    # accept VPN traffic
-    $IPT -A INPUT -i $VPN_IFACE -j ACCEPT
-    $IPT -A OUTPUT -o $VPN_IFACE -j ACCEPT
+    # Allow rip (UDP/520, RIPng: UPD/521)
+    iptables -I INPUT -i $EXT_INET_IFACE -p udp --dport 520 -j ACCEPT
+    iptables -I OUTPUT -o $EXT_INET_IFACE -p udp --sport 520 -j ACCEPT
+    iptables -I INPUT -i $EXT_INET_IFACE -p udp --dport 521 -j ACCEPT
+    iptables -I OUTPUT -o $EXT_INET_IFACE -p udp --sport 521 -j ACCEPT
 
-    # Open ports
-    $IPT -A INPUT -p tcp -d $INET_ADDRESS --dport 1194 -j ACCEPT
-    $IPT -A INPUT -p udp -d $INET_ADDRESS --dport 1194 -j ACCEPT
+    iptables -I INPUT -p igmp -j ACCEPT
+    iptables -I OUTPUT -p igmp -j ACCEPT
+    iptables -I FORWARD -p igmp -j ACCEPT
+
+    # Allow ESP
+    iptables -I FORWARD -p esp -j ACCEPT
+
+    # Allow AH
+    iptables -I FORWARD -p ah -j ACCEPT
+
+    # Allow internet to access http server in dmz
+    iptables -I FORWARD -i $EXT_INET_IFACE -d $DMZ_NET -p tcp --dport http -j ACCEPT
+    iptables -I FORWARD -s $DMZ_NET -o $EXT_INET_IFACE -p tcp --sport http -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
+    # Allow internet to access dns server in dmz
+    iptables -I FORWARD -i $EXT_INET_IFACE -d $DMZ_NET -p udp --dport domain -j ACCEPT
+    iptables -I FORWARD -s $DMZ_NET -o $EXT_INET_IFACE -p udp --sport domain -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
+    # Allow internet to access smtp server in dmz
+    iptables -I FORWARD -i $EXT_INET_IFACE -d $DMZ_NET -p tcp --dport 25 -j ACCEPT
+    iptables -I FORWARD -s $DMZ_NET -o $EXT_INET_IFACE -p tcp --sport 25 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+    iptables -I FORWARD -i $EXT_INET_IFACE -d $DMZ_NET -p tcp --dport submission -j ACCEPT
+    iptables -I FORWARD -s $DMZ_NET -o $EXT_INET_IFACE -p tcp --sport submission -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
+    # Allow bastion host to access ssh server from internet
+    iptables -I FORWARD -s $DMZ_BASTIONHOST -o $EXT_INET_IFACE -p tcp --dport 22 -j ACCEPT
+    iptables -I FORWARD -s $DMZ_BASTIONHOST -o $EXT_INET_IFACE -p tcp --dport 22 -j LOG --log-prefix "Secure Shell: "
+    iptables -I FORWARD -i $EXT_INET_IFACE -d $DMZ_BASTIONHOST -p tcp --sport 22 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
+    # Allow bastion host to access http server from internet
+    iptables -I FORWARD -s $DMZ_BASTIONHOST -o $EXT_INET_IFACE -p tcp --dport http -j ACCEPT
+    iptables -I FORWARD -i $EXT_INET_IFACE -d $DMZ_BASTIONHOST -p tcp --sport http -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
+    # Allow bastion host to access dns server from internet
+    iptables -I FORWARD -s $DMZ_BASTIONHOST -o $EXT_INET_IFACE -p udp --dport domain -j ACCEPT
+    iptables -I FORWARD -i $EXT_INET_IFACE -d $DMZ_BASTIONHOST -p udp --sport domain -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
+    # Allow bastion host to access mail server from internet
+    iptables -I FORWARD -s $DMZ_BASTIONHOST -o $EXT_INET_IFACE -p tcp --dport 25 -j ACCEPT
+    iptables -I FORWARD -i $EXT_INET_IFACE -d $DMZ_BASTIONHOST -p tcp --sport 25 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+    iptables -I FORWARD -s $DMZ_BASTIONHOST -o $EXT_INET_IFACE -p tcp --dport submission -j ACCEPT
+    iptables -I FORWARD -i $EXT_INET_IFACE -d $DMZ_BASTIONHOST -p tcp --sport submission -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
 }
 
-#########
-# CUSTOM TRAFFIC
+internal_router(){
 
-initcustomtraffic(){
-    dnstransfers
-    sshtraffic
-    httptraffic
-    ftptraffic
-    mailtraffic
-    icmptraffic
+
+    # prevent freezes
+    iptables -I INPUT -d $LO_IP -j ACCEPT
+    iptables -I OUTPUT -s $LO_IP -j ACCEPT
+
+    #ICMP toelaten
+    iptables -I INPUT -p icmp -s $DMZ_NET -j ACCEPT
+    iptables -I INPUT -p icmp -s $INT_LAN_NET -j ACCEPT
+    iptables -I FORWARD -p icmp -s $DMZ_NET -j ACCEPT
+    iptables -I FORWARD -p icmp -s $INT_LAN_NET -j ACCEPT
+    iptables -I OUTPUT -p icmp -d $DMZ_NET -j ACCEPT
+    iptables -I OUTPUT -p icmp -d $INT_LAN_NET -j ACCEPT
+
+    #DNS server hilbert (192.168.70.1)
+    iptables -I FORWARD -p udp -d $DMZ_BASTIONHOST --dport domain -j ACCEPT
+    iptables -I FORWARD -p udp -s $DMZ_BASTIONHOST -m conntrack --ctstate ESTABLISHED,RELATED --sport domain -j ACCEPT
+
+    #HTTP
+    iptables -I FORWARD -p tcp -d $DMZ_BASTIONHOST --dport http -j ACCEPT
+    iptables -I FORWARD -p tcp -s $DMZ_BASTIONHOST -m conntrack --ctstate ESTABLISHED,RELATED --sport http -j ACCEPT
+
+    #SSH
+    iptables -I FORWARD -p tcp -d $DMZ_BASTIONHOST --dport ssh -j ACCEPT
+    iptables -I FORWARD -p tcp -s $DMZ_BASTIONHOST -m conntrack --ctstate ESTABLISHED,RELATED --sport ssh -j ACCEPT
+
+    #MAIL
+    iptables -I FORWARD -p tcp -d $DMZ_BASTIONHOST --dport smtp -j ACCEPT
+    iptables -I FORWARD -p tcp -s $DMZ_BASTIONHOST -m conntrack --ctstate ESTABLISHED,RELATED --sport smtp -j ACCEPT
+    iptables -I FORWARD -p tcp -d $DMZ_BASTIONHOST --dport submission -j ACCEPT
+    iptables -I FORWARD -p tcp -s $DMZ_BASTIONHOST -m conntrack --ctstate ESTABLISHED,RELATED --sport submission -j ACCEPT
+    iptables -I FORWARD -p tcp -d $DMZ_BASTIONHOST --dport pop3 -j ACCEPT
+    iptables -I FORWARD -p tcp -s $DMZ_BASTIONHOST -m conntrack --ctstate ESTABLISHED,RELATED --sport pop3 -j ACCEPT
+    iptables -I FORWARD -p tcp -d $DMZ_BASTIONHOST --dport imap -j ACCEPT
+    iptables -I FORWARD -p tcp -s $DMZ_BASTIONHOST -m conntrack --ctstate ESTABLISHED,RELATED --sport imap -j ACCEPT
 }
 
-dnstransfers() {
-    $IPT -A INPUT -p tcp --dport 53 -j ACCEPT # zone transfers
-    $IPT -A INPUT -p udp --dport 53 -j ACCEPT # dns queries
-
-    # Destination NAT
-    $IPT -t nat -A PREROUTING -p udp -i $INET_IFACE -d $INET_ADDRESS --dport 53 -j DNAT --to-destination $DMZ_IP:53
-    $IPT -t nat -A PREROUTING -p tcp -i $INET_IFACE -d $INET_ADDRESS --dport 53 -j DNAT --to-destination $DMZ_IP:53
+init_bridge(){
+    ifconfig $DMZ_IFACE_EXT 0.0.0.0 up
+    ifconfig $DMZ_IFACE_INT 0.0.0.0 up
+    brctl addbr br0
+    brctl stp br0 on # prevent switching loops
+    brctl addif br0 $DMZ_IFACE_EXT
+    brctl addif br0 $DMZ_IFACE_INT
+    ifconfig br0 $DMZ_BASTIONHOST up
 }
 
-sshtraffic() {
-    $IPT -A INPUT -p tcp --dport 22 -j LOG --log-prefix "Logging Secure Shell: "
-    $IPT -A INPUT -p tcp --dport 22 -j ACCEPT
-
-    # Destination NAT
-    $IPT -t nat -A PREROUTING -p tcp -i $INET_IFACE -d $INET_ADDRESS --dport 22 -j DNAT --to-destination $DMZ_IP:22
+reject_undefined(){
+    iptables -A INPUT -j REJECT
+    iptables -A OUTPUT -j REJECT
+    iptables -A FORWARD -j REJECT
 }
-
-httptraffic() {
-    #Destination NAT http & https traffic
-    $IPT -t nat -A PREROUTING -p tcp -i $INET_IFACE -d $INET_ADDRESS -m multiport --dport 80,443 -j DNAT --to-destination $DMZ_IP
-}
-
-ftptraffic(){
-    $IPT -t nat -A PREROUTING -p tcp -i $INET_IFACE -d $INET_ADDRESS --dport 21 -j DNAT --to-destination $DMZ_IP:21
-
-    #Passive connection
-    $IPT -t nat -A PREROUTING -p tcp -i $INET_IFACE -d $INET_ADDRESS --destination-port 62000:64000 -j ACCEPT
-}
-
-mailtraffic() {
-    $IPT -t nat -A PREROUTING -p tcp -i $INET_IFACE -d $INET_ADDRESS --dport 25 -j DNAT --to-destination $LOCAL_IP:25
-    $IPT -t nat -A PREROUTING -p tcp -i $INET_IFACE -d $INET_ADDRESS --dport 143 -j DNAT --to-destination $LOCAL_IP:143
-}
-
-icmptraffic(){
-    $IPT -A OUTPUT -p icmp -m state --state NEW,ESTABLISHED,RELATED -j LOG --log-prefix "Logging ICMP output: "
-    $IPT -A OUTPUT -p icmp -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-
-    $IPT -A INPUT -p icmp -m state --state NEW,ESTABLISHED,RELATED -j LOG --log-prefix "Logging ICMP input: "
-    $IPT -A INPUT -p icmp -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-}
-
-# END CUSTOM TRAFFIC
-#########
 
 case $1 in
-    start)
-        echo "Setting up fullfeatured gateway ... "
-        echo " features: nat, dnat, lan, dmz, vpn, custom "
-        echo ""
-        dropcurrentconfig && echo " -> IPTable configuration flushed."
-        droppolicy && echo " -> IPTable default drop policy."
-        initfirewall && echo " -> IPTables initialized."
-        initnat && echo " -> Network address translation configured."
-        initlan && echo " -> Local LAN ( $LOCAL_NET ) configured."
-        initDMZ && echo " -> DMZ network ( $DMZ_NET ) configured."
-        initvpn && echo " -> Virtual private networking configured."
-        initcustomtraffic && echo " -> Custom traffic initialized."
+    external)
+        echo "Setting up iptables for external router ... "
+        dropcurrentconfig   &&  echo " -> IPTable configuration flushed."
+        acceptpolicy        &&  echo " -> IPTable default accept policy."
+        external_router     &&  echo " -> Setting up external router."
+        reject_undefined    &&  echo " -> Reject undefined."
     ;;
-    startlite)
-        echo "Setting up gateway ... "
-        echo " features: nat, dnat, lan, custom "
-        echo ""
-        dropcurrentconfig && echo " -> IPTable configuration flushed."
-        droppolicy && echo " -> IPTable default drop policy."
-        initfirewall && echo " -> IPTables initialized."
-        initnat && echo " -> Network address translation configured."
-        initlan && echo " -> Local LAN ( $LOCAL_NET ) configured."
-        initcustomtraffic && echo " -> Custom traffic initialized."
+    internal)
+        echo "Setting up iptables for internal router ... "
+        dropcurrentconfig   &&  echo " -> IPTable configuration flushed."
+        acceptpolicy        &&  echo " -> IPTable default accept policy."
+        internal_router     &&  echo " -> Setting up internal router."
+        reject_undefined    &&  echo " -> Reject undefined."
     ;;
-    startdmz)
-        echo "Setting up gateway ... "
-        echo " features: nat, dnat, lan, dmz, custom "
-        echo ""
-        dropcurrentconfig && echo " -> IPTable configuration flushed."
-        droppolicy && echo " -> IPTable default drop policy."
-        initfirewall && echo " -> IPTables initialized."
-        initnat && echo " -> Network address translation configured."
-        initlan && echo " -> Local LAN ( $LOCAL_NET ) configured."
-        initDMZ && echo " -> DMZ network ( $DMZ_NET ) configured."
-        initcustomtraffic && echo " -> Custom traffic initialized."
+    bastion)
+        echo "Setting up bridge for bastion host ... "
+        init_bridge         &&  echo " -> Setting up bridge."
     ;;
-    startvpn)
-        echo "Setting up gateway ... "
-        echo " features: nat, dnat, lan, vpn, custom "
-        echo ""
-        dropcurrentconfig && echo " -> IPTable configuration flushed."
-        droppolicy && echo " -> IPTable default drop policy."
-        initfirewall && echo " -> IPTables initialized."
-        initnat && echo " -> Network address translation configured."
-        initlan && echo " -> Local LAN ( $LOCAL_NET ) configured."
-        initDMZ && echo " -> DMZ network ( $DMZ_NET ) configured."
-        initcustomtraffic && echo " -> Custom traffic initialized."
+    flush)
+        echo "Drop current configuration..."
+        dropcurrentconfig   &&  echo " -> IPTable configuration flushed."
     ;;
-    stop)
-        echo "Drop current gateway configuration..."
-        echo ""
-        dropcurrentconfig && echo " -> IPTable configuration flushed."
+    reset)
+        echo "Drop current configuration..."
+        dropcurrentconfig   &&  echo " -> IPTable configuration flushed."
+        acceptpolicy        &&  echo " -> Accept policy enabled."
     ;;
     restart)
-        $0 stop
-        $0 start
+        $0 flush
     ;;
     *)
-        echo "usage: $0 start|startlite|startdmz|startvpn|stop|restart"
-        echo ""
-        echo " -> start: nat, dnat, lan, dmz, vpn, custom"
-        echo " -> startlite: nat, dnat, lan, custom"
-        echo " -> startdmz: nat, dnat, lan, dmz, custom"
-        echo " -> startvpn: nat, dnat, lan, vpn, custom"
+        echo "usage: $0 external|internal|bastion|reset|flush"
         exit 1
     ;;
 esac
